9ce9c00590b5e03127f598bb807b0530d338f15e
prp-git <paulrp@gmail.com>
2021-12-14 15:04:38 -0300

    Criando classe Livro

--- src/br/com/dio/Programa1.java
+++ src/br/com/dio/Programa1.java
@@ -13,6 +13,10 @@ public class Programa1 {
         System.out.println(animal);
         System.out.println(vet);
 
+        Livro livro = new Livro("Amanhã será outro dia", 300);
+
+        System.out.println(livro);
+
         /*
          * int a = 3; int b = 3; System.out.println("w " +(a+b));
          */
@@ -23,4 +27,38 @@ public class Programa1 {
 class Veterinario {
     private String nome;
     private String telefone;
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
 }
